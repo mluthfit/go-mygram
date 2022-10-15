42e6264a7e15a403f766397e8aa35100258f3580
Muhammad Luthfi <mluthfitaufiq@gmail.com>
2022-10-16 05:50:57 +0700

    Add user register handler

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"errors"
+	"go-mygram/helpers"
 	"go-mygram/models"
 	"net/http"
 
@@ -26,4 +28,37 @@ func (s *Server) UserRegister(ctx *gin.Context) {
 		"id":       user.ID,
 		"username": user.Username,
 	})
+}
+
+func (s *Server) UserLogin(ctx *gin.Context) {
+	var user models.User
+
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		resError(ctx, http.StatusBadRequest, err)
+		return
+	}
+
+	var payloadPass = user.Password
+	if err := s.db.Debug().Where("email = ?", user.Email).
+		Take(&user).Error; err != nil {
+		err = errors.New("invalid email or password")
+		resError(ctx, http.StatusUnauthorized, err)
+		return
+	}
+
+	if comparePass := helpers.ComparePass(
+		[]byte(user.Password), []byte(payloadPass),
+	); !comparePass {
+		var err = errors.New("invalid email or password")
+		resError(ctx, http.StatusUnauthorized, err)
+		return
+	}
+
+	ctx.JSON(200, gin.H{
+		"token": helpers.GenerateToken(user.ID, user.Email),
+	})
+}
+
+func (s *Server) UserUpdate(ctx *gin.Context) {
+
 }
