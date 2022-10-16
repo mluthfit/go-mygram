9463ba8b559910bbcbbc89bdc6ae2161d3304ad0
Muhammad Luthfi <mluthfitaufiq@gmail.com>
2022-10-16 08:16:49 +0700

    Change validation for age

--- models/user.go
+++ models/user.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"errors"
 	"go-mygram/helpers"
 
 	"github.com/asaskevich/govalidator"
@@ -12,10 +13,14 @@ type User struct {
 	Username string `json:"username" gorm:"uniqueIndex;not null" valid:"required"`
 	Email    string `json:"email" gorm:"uniqueIndex;not null" valid:"required,email"`
 	Password string `json:"password" gorm:"not null" valid:"required,minstringlength(6)"`
-	Age      uint   `json:"age" gorm:"not null" binding:"gt=8" valid:"required,numeric"`
+	Age      uint   `json:"age" gorm:"not null" valid:"required,numeric"`
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) error {
+	if u.Age <= 8 {
+		return errors.New("age must be greater than 8")
+	}
+
 	if _, err := govalidator.ValidateStruct(u); err != nil {
 		return err
 	}
